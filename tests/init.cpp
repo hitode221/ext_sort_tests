96801843178297e95537613fa167b391c418887b
hitode221 <marina.dr98@mail.ru>
2016-11-20 12:51:00 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,4 +5,17 @@
 
 SCENARIO("sort", "[sort]") {
 	ext_sort("random.txt", 256);
+	std::ifstream expected("expected.txt");
+	std::ifstream output("result.txt");
+	std::string first, second;
+	bool flag = true;
+	while (expected || output) {
+		std::getline(expected, first);
+		std::getline(output, second);
+		if (first != second) {
+			flag = false;
+			break;
+		}
+	}
+	REQUIRE(flag);
 }
