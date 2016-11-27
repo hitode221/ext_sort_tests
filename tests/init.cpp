e20e99ffa0b3cc479f955f303219488523b9dd78
hitode221 <marina.dr98@mail.ru>
2016-11-27 23:16:27 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 #include <fstream>
 
-SCENARIO("sort", "[sort]") {
-	/*("random.txt", 1);
-	std::ifstream expected("expected.txt");
-	std::ifstream output("result.txt");
+SCENARIO("sort 8mb", "[sort]") {
+	ext_sort("8", "sort_8.txt", 1);
+	std::ifstream expected("sort8.txt");
+	std::ifstream output("sort_8.txt");
 	std::string first, second;
 	bool flag = true;
 	while (expected || output) {
@@ -17,6 +17,5 @@ SCENARIO("sort", "[sort]") {
 			break;
 		}
 	}
-	REQUIRE(flag);*/
-	ext_sort("15", "result", 4);
+	REQUIRE(flag);
 }
