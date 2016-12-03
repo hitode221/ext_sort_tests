270ad058cdbc26560aa4cc86d3b1291cf18ed2b7
hitode221 <marina.dr98@mail.ru>
2016-12-03 14:50:56 +0300

    Update ext_sort.h

--- include/ext_sort.h
+++ include/ext_sort.h
@@ -10,9 +10,8 @@
 #include <sstream>
 
 struct man {
-	std::string last_name, first_name, file_name;
+	std::string last_name, first_name;
 	size_t year;
-	std::ifstream* file;
 	bool operator <(const man & data) const{
 		return  first_name < data.first_name;
 	}
@@ -27,36 +26,7 @@ bool Sort(man a, man b) {
 std::string generate_name(size_t i) {
 	std::stringstream ss;
 	ss << i << ".txt";
-	std::string result = ss.str();
-	return result;
-}
-void merge_all_files(size_t number_of_files, std::string result_name) {
-	std::ofstream result(result_name);
-	std::priority_queue<man> people;
-	man temp;
-	int u = 0;
-	for (size_t i = 0; i < number_of_files; ++i) {
-		temp.file = new std::ifstream(generate_name(i));
-		temp.file_name = generate_name(i);
-		*temp.file >> temp.last_name >> temp.first_name >> temp.year;
-		people.push(temp);
-	}
-	man temp1;
-	while (!people.empty()) {
-		temp1 = people.top();
-		people.pop();
-		result << temp1.last_name << " " << temp1.first_name << " " << temp1.year << std::endl;
-		if (*temp1.file) {
-			(*temp1.file) >> temp1.last_name >> temp1.first_name >> temp1.year;
-			if (*temp1.file) {
-				people.push(temp1);
-			}
-			else {
-				(*temp1.file).close();
-				remove(temp1.file_name.c_str());
-			}
-		}
-	}
+	return ss.str();
 }
 
 void merge(size_t number_of_files_, std::string result_name) {
@@ -98,19 +68,21 @@ void merge(size_t number_of_files_, std::string result_name) {
 	}
 }
 
-void ext_sort(std::string file_name, std::string result_file_name, size_t size_of_block) {
+void ext_sort(std::string && file_name, std::string && result_file_name, size_t && size_of_block) {
 	std::fstream fin(file_name);
 	if (!fin.is_open()) return;
 	size_t number_of_files = 0, size_of_block_ = size_of_block * 1024 * 1024;
+	size_t memory = size_of_block_ / 105;
 	std::vector<man> people;
+	people.reserve(memory);
 	man temp;
 	while (!fin.eof()) {
 		std::ofstream fout(generate_name(number_of_files));
-		do {
+		for (size_t i = 0; i < memory; i++){
 			if (fin.eof()) break;
 			fin >> temp.last_name >> temp.first_name >> temp.year;
 			people.push_back(temp);
-		} while ((sizeof(std::vector<man>) + sizeof(man) * (people.size()+1)) < size_of_block_);
+		} //while ((sizeof(std::vector<man>) + sizeof(man) * (people.size()+1)) < size_of_block_);
 		sort(people.begin(), people.end(), Sort);
 		for (size_t j = 0; j < people.size(); j++)
 			fout << people[j].last_name << " " << people[j].first_name << " " << people[j].year << std::endl;
