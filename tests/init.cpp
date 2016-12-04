aa3d8d88277aaba0e62e7db2d20ffb4ce7614a20
hitode221 <marina.dr98@mail.ru>
2016-12-04 19:17:44 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -2,9 +2,14 @@
 #include <catch.hpp>
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
-/*SCENARIO("sort 8mb", "[sort]") {
+SCENARIO("sort 8mb", "[sort]") {
+	auto start_time = std::chrono::steady_clock::now();
 	ext_sort("8", "sort_8.txt", 1);
+	auto end_time = std::chrono::steady_clock::now();
+	auto elapsed_s = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+	std::cout << "8mb: " << elapsed_s.count() << " ms" << std::endl;
 	std::ifstream output("sort_8.txt");
 	std::string temp;
 	size_t t = 0;
@@ -18,7 +23,11 @@
 }
 
 SCENARIO("sort 15mb", "[sort]") {
+	auto start_time = std::chrono::steady_clock::now();
 	ext_sort("15", "sort_15.txt", 4);
+	auto end_time = std::chrono::steady_clock::now();
+	auto elapsed_s = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+	std::cout << "15mb: " << elapsed_s.count() << " ms" << std::endl;
 	std::ifstream output("sort_15.txt");
 	std::string temp;
 	size_t t = 0;
@@ -29,11 +38,15 @@ SCENARIO("sort 15mb", "[sort]") {
 	}
 	REQUIRE(t == 792011+1);
   	output.close();
-}*/
+}
 
 SCENARIO("sort 32mb", "[sort]") {
+	auto start_time = std::chrono::steady_clock::now();
 	ext_sort("32", "sort_32.txt", 17);
-	/*std::ifstream output("sort_32.txt");
+	auto end_time = std::chrono::steady_clock::now();
+	auto elapsed_s = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+	std::cout << "32mb: " << elapsed_s.count() << " ms" << std::endl;
+	std::ifstream output("sort_32.txt");
 	std::string temp;
 	size_t t = 0;
 	std::getline(output, temp);
@@ -42,6 +55,6 @@ SCENARIO("sort 32mb", "[sort]") {
 		++t;
 	}
 	REQUIRE(t == 1637032+1);
-  	output.close();*/
+  	output.close();
 }
 
