ca30d7a4d268a2f2a11a38865d1fd7e9540759cd
hitode221 <marina.dr98@mail.ru>
2016-12-04 17:41:48 +0300

    Update ext_sort.h

--- include/ext_sort.h
+++ include/ext_sort.h
@@ -74,12 +74,12 @@ void ext_sort(std::string && file_name, std::string && result_file_name, size_t 
 	std::vector<man> people;
 	people.reserve(memory);
 	man temp;
-	while (!fin.eof()) {
+	while (fin) {
 		std::ofstream fout(generate_name(number_of_files));
 		for (size_t i = 0; i < memory; ++i){
-			if (fin.eof()) break;
 			fin >> temp.last_name >> temp.first_name >> temp.year;
 			people.push_back(temp);
+			if (!fin) break;
 		} //while ((sizeof(std::vector<man>) + sizeof(man) * (people.size()+1)) < size_of_block_);
 		sort(people.begin(), people.end(), Sort);
 		for (size_t j = 0; j < people.size(); ++j)
